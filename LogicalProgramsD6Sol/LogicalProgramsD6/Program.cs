2f4ef6e1332541d939e4ebbf016ba254621a5e02
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-10-11 16:57:30 +0530

    Update Program.cs

--- LogicalProgramsD6Sol/LogicalProgramsD6/Program.cs
+++ LogicalProgramsD6Sol/LogicalProgramsD6/Program.cs
@@ -6,8 +6,11 @@ class Problem
     static void Main()
     {
         // Console.Write("Welcome to logical problems program");
-        // Problem1.Fibonacci();
-        //Problem2.PerfectNumber();
+        Problem1.Fibonacci();
+        Problem2.PerfectNumber();
         Problem3.PrimeNumber();
+        Problem4.ReverseNo();
+        Problem5.CuponGenerator();
+        problem6.Stopwatch();
     }
 }
